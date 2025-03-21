3d8b96b485d7b9b2b378ca2429fe64aff99bbdf8
Brian Karmo <karmobrian@gmail.com>
2025-03-21 06:25:33 -0400

    Add files via upload

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,3 +1,4 @@
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
@@ -6,5 +7,5 @@ module.exports = {
       signature: ["Great Vibes"],
     },
   },
-  plugins: [],
+  plugins: [], // ❌ Remove "@tailwindcss/line-clamp" from plugins
 };
